118795856346a27df50d2fbe0fcca781368298b4
Tereshkov Sergei[D] <rat87@mail.ru>
2020-04-09 00:30:22 +0300

    feat(components): add MapBoxGl into MapPage component

--- src/components/MapPage/MapPage.jsx
+++ src/components/MapPage/MapPage.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import { Header } from '../Header';
+import mapboxgl from 'mapbox-gl';
 
 export class MapPage extends React.Component {
 	getPage = (page) => {
 		this.props.getPage(page);
 	};
+
+	myMapRef = React.createRef();
+
+	componentDidMount() {
+		mapboxgl.accessToken =
+			'pk.eyJ1IjoidGVyYXRyb24iLCJhIjoiY2s4c3dyOGZ4MDNoMjNlbGtvYzJ3NzBsciJ9.h0f9Px2X_1Go39lBV5kq7Q';
+		this.map = new mapboxgl.Map({
+			container: this.myMapRef.current,
+			style: 'mapbox://styles/mapbox/streets-v9',
+			center: [30.2656504, 59.8029126],
+			zoom: 15,
+		});
+	}
+
 	render() {
 		return (
 			<div className="app">
 				<Header getPage={this.getPage} />
-				<div className="map-page">Содержимое карты</div>
+				<div className="map-page" ref={this.myMapRef}></div>
 			</div>
 		);
 	}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import { theme } from 'loft-taxi-mui-theme';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import './index.css';
+
+
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<MuiThemeProvider theme={theme}>
+			<App />
+		</MuiThemeProvider>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
