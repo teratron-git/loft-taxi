cdb5ad3ef4d7b1ad983ba6e4960f4a8337e913c3
Tereshkov Sergei[D] <rat87@mail.ru>
2020-05-04 22:26:07 +0300

    feat: validate profile  form via redux-form

--- src/components/Dashboard/ProfilePage/ProfilePage.jsx
+++ src/components/Dashboard/ProfilePage/ProfilePage.jsx
@@ -6,24 +6,62 @@ import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
-import NumberFormat from 'react-number-format';
+// import NumberFormat from 'react-number-format';
+// import Input from '@material-ui/core/Input';
 import { MCIcon } from 'loft-taxi-mui-theme';
 import { bindActionCreators } from 'redux';
+import { Field, reduxForm } from 'redux-form';
 import { actions } from '../../../store/profile/actions';
 import { Preloader } from '../../shared/Preloader';
 import { getCardName, getCardNumber, getError, getCardCvv, getIsCard, getIsCardLoading, getCardExpiry } from '../../../store/profile/selectors'
 
 let { card, cardErrorReset } = actions;
-
 const st = classNames.bind(styles);
+
+const validate = values => {
+	const errors = {}
+	console.log('values', values)
+	!values.cardName
+		? (errors.cardName = 'Поле не должно быть пустым')
+		: (/^.*[^A-zА-яЁё\s].*$/i.test(values.cardName)
+			? errors.cardName = 'Используйте только буквы'
+			: errors.cardName = null)
+
+	!values.cardNumber
+		? (errors.cardNumber = 'Поле не должно быть пустым')
+		: (!/^\d{16}$/i.test(values.cardNumber)
+			? errors.cardNumber = 'Введите 16 цифр'
+			: errors.cardNumber = null)
+
+	!values.cardExpiry
+		? (errors.cardExpiry = 'Поле не должно быть пустым')
+		: (!/^\d{4}$/i.test(values.cardExpiry)
+			? errors.cardExpiry = 'Введите 4 цифры'
+			: errors.cardExpiry = null)
+
+	!values.cardCvv
+		? (errors.cardCvv = 'Поле не должно быть пустым')
+		: (!/^\d{3}$/i.test(values.cardCvv)
+			? errors.cardCvv = 'Введите 3 цифры'
+			: errors.cardCvv = null)
+
+	return errors;
+}
+
+const customTextField = ({ input, type, placeholder, id, className, label, fullWidth, inputProps, meta: { touched, error }, ...rest }) => {
+	return (<><TextField {...input} placeholder={placeholder} type={type} id={id} className={className} label={label} fullWidth={fullWidth} inputProps={inputProps} />
+		<span className={st('validateError')}>{touched && error}</span>
+	</>)
+}
 
 const ProfilePage = (props) => {
 	let [cardName, setCardName] = useState(props.cardName);
 	let [cardNumber, setCardNumber] = useState(props.cardNumber);
 	let [cardExpiry, setCardExpiry] = useState(props.cardExpiry);
 	let [cardCvv, setCardCvv] = useState(props.cardCvv);
+	console.log('props', props)
 
-	let { card, cardErrorReset, error, isCardLoading, isCard } = props;
+	let { card, cardErrorReset, serverError, isCardLoading, isCard } = props;
 
 	const changeCardNameHandler = (e) => {
 		setCardName(e.target.value);
@@ -61,10 +99,10 @@ const ProfilePage = (props) => {
 						<Paper elevation={13} className={styles.paper}>
 							<MCIcon className={styles.MCIcon} />
 							<form className={styles.form} onSubmit={submitHandler}>
-								<TextField
+								<Field
 									className={st('font-size')}
 									type="text"
-									label="Имя владельца"
+									label="Имя владельца*"
 									placeholder="Имя владельца"
 									name="cardName"
 									value={cardName}
@@ -73,11 +111,13 @@ const ProfilePage = (props) => {
 									autoComplete="cc-name"
 									fullWidth
 									required
+									component={customTextField}
+									inputProps={{ value: cardName }}
 								/>
-								<NumberFormat
+								<Field
 									className={st('font-size')}
 									customInput={TextField}
-									label="Номер карты"
+									label="Номер карты*"
 									placeholder="Номер карты"
 									name="cardNumber"
 									value={cardNumber}
@@ -86,12 +126,13 @@ const ProfilePage = (props) => {
 									autoComplete="cc-number"
 									fullWidth
 									required
-									format="#### #### #### ####"
+									component={customTextField}
+									inputProps={{ maxLength: "16", value: cardNumber }}
 								/>
-								<NumberFormat
+								<Field
 									className={st('font-size')}
 									customInput={TextField}
-									label="Дата окончания действия"
+									label="Дата окончания действия*"
 									placeholder="мм/гг"
 									name="cardExpiry"
 									value={cardExpiry}
@@ -100,13 +141,14 @@ const ProfilePage = (props) => {
 									autoComplete="cc-exp"
 									fullWidth
 									required
-									format="##/##"
+									component={customTextField}
+									inputProps={{ maxLength: "4", value: cardExpiry }}
 								/>
-								<NumberFormat
+								<Field
 									className={st('font-size')}
 									type="password"
 									customInput={TextField}
-									label="CVV"
+									label="CVV*"
 									placeholder="CVV"
 									name="cardCvv"
 									value={cardCvv}
@@ -115,9 +157,10 @@ const ProfilePage = (props) => {
 									autoComplete="cc-csc"
 									fullWidth
 									required
-									format="###"
+									component={customTextField}
+									inputProps={{ maxLength: "3", value: cardCvv }}
 								/>
-								<span className={st({ 'error': !isCard, 'no-error': isCard })}>{error}</span>
+								<span className={st({ 'error': !isCard, 'no-error': isCard })}>{serverError}</span>
 								{isCardLoading ? (<div className={st('preloader-position')}><Preloader /></div>)
 									: (
 										<Button
@@ -149,7 +192,7 @@ export const mapStateToProps = (state) => {
 		cardCvv: getCardCvv(state),
 		isCard: getIsCard(state),
 		isCardLoading: getIsCardLoading(state),
-		error: getError(state),
+		serverError: getError(state),
 	};
 };
 
@@ -160,4 +203,6 @@ export const mapDispatchToProps = (dispatch) => {
 	};
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(ProfilePage);
+export default connect(mapStateToProps, mapDispatchToProps)(reduxForm({
+	form: 'ProfilePage', validate
+})(ProfilePage));
